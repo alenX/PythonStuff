ec155e45031e78c71c4c8ad114be7f06bd191a6c
alenX <genhaoai@gmail.com>
2014-05-23 11:43:29 +0800

    add function comment

--- ConMysql.py
+++ ConMysql.py
@@ -3,13 +3,16 @@ __author__ = 'wangss'
 import MySQLdb
 
 
-class ConMysql(object):  #the class of conn
+class ConMysql(object):
+    #the class of conn
     def __init__(self, host, user, passwd, db, charset):
+        "init of db conn"
         conn = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, charset=charset)
         self.cursor = conn.cursor()
         self.conn = conn
 
     def deletesql(self, sqlstr):
+        "delete sql function"
         self.cursor.execute(sqlstr)
         self.conn.commit()
         self.cursor.close()
