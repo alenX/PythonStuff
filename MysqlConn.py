5d4ee8cb02061d68865bd34c1561210ae266ce09
alenX <genhaoai@gmail.com>
2014-05-23 11:12:08 +0800

    a test of mysql conn

--- MysqlConn.py
+++ MysqlConn.py
@@ -1,1 +1,23 @@
 __author__ = 'wangss'
+import MySQLdb
+
+
+def conn():
+    try:
+        conn = MySQLdb.connect(host="localhost", user="root", passwd="1128", db="test", charset="utf8")
+        cursor = conn.cursor()
+        count = cursor.execute("SELECT * FROM STUDENT")
+
+        result = cursor.fetchmany(count)
+        for i in result:
+            for a in i:
+                print(a)  #the charset
+        cursor.close()
+        conn.close()
+    except MySQLdb.Error, e:
+        print("%d,%s" % (e.args[0], e.args[1]))
+
+
+if __name__ == '__main__':
+    conn()
+
