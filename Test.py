fe1b4e3f1c91b553f1c92851f1ec748a102ff438
alenX <genhaoai@gmail.com>
2014-05-23 10:36:17 +0800

    used for db controller

--- Test.py
+++ Test.py
@@ -5,5 +5,14 @@ def test():
     print("")
 
 
+class Conn(object):
+    "connect  base class"
+
+    def __init__(self, name, user, password):
+        self.name = name
+        self.user = user
+        self.password = password
+
+
 if __name__ == '__main__':
     test()
