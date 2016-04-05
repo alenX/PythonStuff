36a3d3aa811d1797e9a4d0d7bca7c79457726571
alenX <alenX@users.noreply.github.com>
2016-04-05 15:19:53 +0800

    Update delete_tomcat_logs.py

--- delete_tomcat_logs.py
+++ delete_tomcat_logs.py
@@ -5,10 +5,10 @@ re_pre = '^apache-tomcat-(\w+)'
 # for root,dirs,dd in os.walk(log_path):
 #     for d in dirs:
 #         print(d)
-tomcat_dir = []
-for i in os.listdir(log_path):
-    if re.match(re_pre,i) is not None:
-        tomcat_dir.append(i)
+tomcat_dir = [i for i in os.listdir(log_path) if re.match(re_pre,i) is not None]
+# for i in os.listdir(log_path):
+#     if re.match(re_pre,i) is not None:
+#         tomcat_dir.append(i)
 for i in tomcat_dir:
     logs = log_path+'\\'+i+'\\logs'
     for root,dirs,files in os.walk(logs,topdown=False):
